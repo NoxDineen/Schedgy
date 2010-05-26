9e3fc44bb6ff10145159528810d4421a058773bf
bcoe <bencoe@gmail.com>
2010-05-26 14:14:43 -0400

    Adding stubbed out files for schedgy going to begin by working on JavaScript.

---
+++ public/javascripts/schedgy.js
@@ -0,0 +1,18 @@
+/**
+ * Schedgy, the simple scheduling tool.
+ * MIT Licensed 2010
+ * Programmed By: Benjamin E. Coe
+ */
+
+
+/**
+ * The calendar widget.
+ *
+ * @param {jQuery object} $element an element to attach the calendar object to.
+ * @retrun calendar is initialized and attached to the element provided.
+ */
+var Calendar = Class.extend({
+	init: function($element) {
+		console.log('Calendar constructor.');
+	}
+});
