6889d50ec1da2865e87073bb72e2793f78774b34
bcoe <bencoe@gmail.com>
2010-05-26 13:58:15 -0400

    First commit of schedgy.

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.cookie_secret = '2fda6f7e3ab6669f77264ff0c8fda9e14dd24500c2623e85797047c1056c4eb43aaa391d0f192e1db40949221eebdf73ab80a1cfe64d6f30133f7847f4bdb900'
