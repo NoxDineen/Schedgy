87fa6f118c1dc202ed2c3dfe5b96b3bace7e17c3
Ben Coe <bencoe@bencoe.2ndsiteinc.com>
2010-07-06 13:44:51 -0400

    Added validation for the user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ActiveRecord::Base
+  
+  # Relationships.
   has_many :roles
   has_many :role_types, :through => :roles
-    
+  
+  # Validation.
+  validates_uniqueness_of :email
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  validates_length_of :email, :in => 1..50
+  validates_length_of :first_name, :in => 1..50
+  validates_length_of :last_name, :in => 1..50
+      
   def get_payload
     return_me = {
       :first_name => self.first_name,
