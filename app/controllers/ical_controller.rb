a04cb874003e93b992a36bf8ee7723235782f5fa
Owen Jacobson <owen@freshbooks.com>
2010-10-14 12:29:30 -0400

    Added support for iCalendar output via a separate controller (ical_controller.rb)

---
+++ app/controllers/ical_controller.rb
@@ -0,0 +1,49 @@
+require 'icalendar'
+
+class IcalController < ApplicationController
+  include Icalendar
+
+  def index
+    # begin
+      if params['date']
+        this_month = Time.parse params['date']
+      else
+        this_month = Time.now;
+      end
+    
+      if params['email']
+        users = User.find(:all, :conditions => {:email => params['email']})
+      else
+        users = User.find(:all)
+      end
+    
+      start_month = this_month - 1.month
+      end_month = this_month + 3.months
+
+      days = Day.find(:all, :conditions => {:date => start_month..end_month})
+    
+      calendar = Calendar.new
+      days.each do |day|
+        day.assigned_users.select {|u| users.include? u}.each do |user|
+          calendar.event do
+            dtstart day.date
+            dtend day.date
+            summary "#{user.first_name} #{user.last_name} is on support"
+            add_attendee user.email
+            
+            alarm do
+              summary "#{user.first_name} #{user.last_name} is on support"
+              trigger "-PT15H"
+            end
+          end
+        end
+      end
+    
+      render :status => 200, :content_type => "text/plain",
+             :text => calendar.to_ical
+    # rescue ActiveRecord::RecordNotFound
+    #   render :status => 404, :content_type => "text/plain",
+    #          :text => "No user #{params['user']} found."
+    # end
+  end
+end
